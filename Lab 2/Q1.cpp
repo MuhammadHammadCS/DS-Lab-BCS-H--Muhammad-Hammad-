b41626c387d3c9ac181665420b460a81422b015b
Muhammad Hammad <hammadamin73@gmail.com>
2025-09-01 12:13:43 +0500

    Update Q1.cpp

--- Lab 2/Q1.cpp
+++ Lab 2/Q1.cpp
@@ -23,7 +23,35 @@ int main()
     {
         cout << "arr[" << i << "]: " << arr[i] << endl;
     }
-    
+    char choice = 'y';
+    int pos;
+    int val;
+    while(choice == 'y' || choice == 'Y')
+    {
+        cout << "Enter the index to change value at from(1 to " << size << ") : ";
+        cin >> pos;
+        if(pos > 0 && pos <= size)
+        {
+            cout << "Enter the value: ";
+            cin >> val;
+            arr[pos-1] = val;
+        }
+        else
+        {
+            cout << "Invalid Index!!" << endl;
+        }
+        cout << "do you want to update any value in the array(y for yes || n for no): ";
+        cin >> choice;
+    }
+    if(choice == 'n' || choice == 'N')
+    {
+        cout << "No array updated" << endl;
+    }
+    cout <<"\nDisplaying...\n";
+    for (int i = 0; i < size; i++)
+    {
+        cout << "arr[" << i << "]: " << arr[i] << endl;
+    }
     delete[] arr;
     
 }
