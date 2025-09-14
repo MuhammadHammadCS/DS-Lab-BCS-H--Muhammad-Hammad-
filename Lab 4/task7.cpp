d371d20625bc188e428b6b63a4cce82da1c49391
Muhammad Hammad <hammadamin73@gmail.com>
2025-09-14 17:23:42 +0500

    Update task7.cpp

--- Lab 4/task7.cpp
+++ Lab 4/task7.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 void shellSort(int arr[], int size)
 {
-    int count = 0;
+    int swap = 0;
+    int comp = 0;
     for (int gap = size/2; gap > 0; gap /= 2)
     {
         for (int i = gap; i < size; i++)
@@ -15,36 +16,40 @@ void shellSort(int arr[], int size)
             {
                 arr[res] = arr[res - gap];
                 res -= gap;
+                swap++;
+                comp++; 
             }
             arr[res] = temp;
-            count++;
         }   
     }
-    cout << "\nComplexity Analysis for Shell Sort :\nnumber of comparisons and swapped: " << count << endl;
+    cout << "\nComplexity Analysis for Shell Sort :\nnumber of comparisons: " << comp << " and swapped: " << swap << endl;
 }
 
 void bubbleSort(int arr[], int size)
 {
-    int count = 0;
+    int swap = 0;
+    int comp = 0;
     for (int i = 0; i < size; i++)
     {
         for (int j = 0; j < size-1; j++)
         {
+            comp++;
             if(arr[j] > arr[j+1])
             {
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
-                count++;
+                swap++;
             }
         }
     } 
-    cout << "\nComplexity Analysis for Bubble Sort:\nnumber of comparisons and swapped: " << count << endl;
+    cout << "\nComplexity Analysis for Bubble Sort :\nnumber of comparisons: " << comp << " and swapped: " << swap << endl;
 }
 
 void insertionSort(int arr[], int size)
 {
-    int count;
+    int swap = 0;
+    int comp = 0;
     for (int i = 1; i < size; i++)
     {
         int curr = arr[i];
@@ -54,11 +59,12 @@ void insertionSort(int arr[], int size)
         {
             arr[prev + 1]  = arr[prev];
             prev--;
+            swap++;
+            comp++;
         }
         arr[prev + 1] = curr;
-        count++;
     }
-    cout << "\nComplexity Analysis for Insertion Sort:\nnumber of comparisons and swapped: " << count << endl; 
+    cout << "\nComplexity Analysis for Insertion Sort :\nnumber of comparisons: " << comp << " and swapped: " << swap << endl; 
 }
 
 void display(int arr[], int n)
@@ -82,13 +88,27 @@ int main()
         cout << "Enter value for element " << i << ": ";
         cin >> arr[i];
     }
+    int *arr1 = new int[n];
+    int *arr2 = new int[n];
+    int *arr3 = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        arr1[i] = arr2[i] = arr3[i] = arr[i];
+    }
+    
+     
     cout << endl;
     cout << "Unsorted Array\n";
     display(arr, n);
-    bubbleSort(arr,n);
-    insertionSort(arr,n);
-    shellSort(arr, n);
+    bubbleSort(arr1,n);
+    insertionSort(arr2,n);
+    shellSort(arr3, n);
     cout << "\nSorted Array\n";
     display(arr, n);
     delete[] arr;
+    delete[] arr1;
+    delete[] arr2;
+    delete[] arr3;
+
 }
